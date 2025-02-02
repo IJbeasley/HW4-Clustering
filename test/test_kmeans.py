66fe37ec32fe87534226c539214c0809ef290700
Isobel Beasley <isobel.beasley@ucsf.edu>
2025-02-01 21:33:09 -0800

    Add initial unit test for invalid k value

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,1 +1,19 @@
 # Write your k-means unit tests here
+import pytest
+from cluster import kmeans.KMeans
+
+
+def bad_k_value_init()
+    """
+    
+    """
+    
+    try:
+      
+      KMeans(k = 0)
+      assert False, "Problem: no error"
+    
+    except ValueError as e:
+      assert str(e) == "The number of requested clusters (k) must be a positive integer >= 1.", "Problem: wrong error"
+      
+  
