66dc08b82ef41765714883e869c4ce9824231b3d
Isobel Beasley <isobel.beasley@ucsf.edu>
2025-02-12 00:36:30 -0800

    Fix implementation difference between cluster silhouette score and sklearn

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from scipy.spatial.distance import cdist
 
 
@@ -29,11 +30,34 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+       
+        # Are provided X and y arrays valid: checks?
+        if X.ndim != 2:
+          
+           raise ValueError("Provided mat should be a 2D matrix")
+        
+        if y.ndim != 1:
+          
+           raise ValueError("Provided labels should be a 1D array")
+        
+        if len(y) != X.shape[0]:
+         
+           raise ValueError("Provided X and y arrays are not compatible. Number of rows in X should = number of elements in Y")
         
   
         # What are the clusters?
         cluster_labels = np.unique(y)
         cluster_labels = np.sort(cluster_labels)
+        
+        # Are cluster labels valid: checks? 
+        if len(cluster_labels) < 2:
+           
+           raise ValueError("Number of clusters must be >= 2")
+         
+        
+        if len(cluster_labels) > X.shape[0] - 1: 
+          
+           raise ValueError("Silhouette score is only defined if number of clusters is <= number of observations - 1")
       
         # Match each observation to a cluster
         cluster_dict = {
@@ -70,27 +94,34 @@ class Silhouette:
                    cluster_points = [p for p in cluster_points if p != observation]
 
                 # How far is observation from points in this cluster on average?
-                # If the only point in this cluster is the observation, then avg_cluster_distance is 0
+                
+                # If the only point in the same cluster as the observation, then let's make silhouette score for
+                # this observation point 0. 
+                # Achieve this by setting average cluster distance to Inf. 
                 if not cluster_points:
-                   avg_clust_distance[observation, cluster_id] = 0
+                   avg_clust_distance[observation, cluster_id] = float('inf')
                 else:
-                  avg_clust_distance[observation, cluster_id] = all_distances[observation, cluster_points].mean()
+                   avg_clust_distance[observation, cluster_id] = all_distances[observation, cluster_points].mean()
           
           
             # How far is that point from other points in the same cluster (on average)?
             # Intra-cluster distance from this point:
             intra_dist = avg_clust_distance[observation, observation_cluster]
           
-            # How far is the smallest mean distance to a different cluster?
-            # Inter-cluster distance from this point: 
-            inter_dist = np.delete(
-                                   avg_clust_distance[observation], 
-                                   observation_cluster
-                                  )
-          
-            min_inter_dist = inter_dist.min()
+            if math.isinf(intra_dist):
+               score[observation] = 0
             
-            score[observation] = (min_inter_dist - intra_dist) / max(min_inter_dist, intra_dist)
+            else:
+               # How far is the smallest mean distance to a different cluster?
+               # Inter-cluster distance from this point: 
+               inter_dist = np.delete(
+                                      avg_clust_distance[observation], 
+                                      observation_cluster
+                                      )
+          
+               min_inter_dist = inter_dist.min()
+
+               score[observation] = (min_inter_dist - intra_dist) / max(min_inter_dist, intra_dist)
         
         return score
 
--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -6,12 +6,13 @@ from sklearn.metrics import silhouette_score
 
 def test_silhouette_score():
     """
-    Unit test that checks that compares the Silhouette scoring function `sklearn.metrics.silhouette_score`. 
+    Unit test that checks compares the Silhouette scoring function in our cluster module
+    to `sklearn.metrics.silhouette_score`. 
     """
     
     # Firstly, let's make dataset to compare silhouette score with
     
-    # Test dataset of 10 observations, measured in 2 dimensions, with small sd
+    # Test dataset of 10 observations, measured in 4 dimensions, with sd of 1
     test_clusters, test_cluster_labels  =  utils.make_clusters(n=10,
                                                                m=4,
                                                                scale = 1,
@@ -26,9 +27,4 @@ def test_silhouette_score():
     cluster_silhouette = silhouette.Silhouette()
     cluster_silhouette_scores = cluster_silhouette.score(test_clusters, test_cluster_labels)
     
-    # My module scores are 0.1 higher than sklearn 
-    # I think cause of the way I'm dealing with edge cases 
-    # print(sklearn_scores)
-    # print(cluster_silhouette_scores.mean())
-    
-    assert np.allclose(sklearn_scores, cluster_silhouette_scores.mean(), atol = 0.1), "cluster module silhouette scores are not similar to sklearn scores"
+    assert np.allclose(sklearn_scores, cluster_silhouette_scores.mean()), "cluster module silhouette scores are not similar to sklearn scores"
