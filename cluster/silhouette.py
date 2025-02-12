d6a2ced4dd0ee06feae6d640bb2d22e516d92cd9
Isobel Beasley <isobel.beasley@ucsf.edu>
2025-02-11 22:12:14 -0800

    Add initial silhoutte score function

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -24,3 +24,35 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+        
+  
+      
+      cluster_labels = np.unique(y)
+      
+      cluster_dict = {
+                     label: np.where(cluster_id == label)[0].tolist() for cluster_id in cluster_labels
+                     }
+                     
+      # For a given point
+      
+      for observation in range(0, len(y)):
+        
+          # What cluster is that point in?
+          cluster_id = y[observation]
+          cluster = cluster_dict.get(cluster, [])
+        
+          # How far is that point from other points in the same cluster (on average)?
+          # Intra-cluster distance from this point:
+          
+          intra_dist = cdist(X[observation, ], 
+                             np.delete(X[cluster,], observation, axis = 0)
+                             )
+                             
+          mean_intra_dist = intra_dist.mean()
+          
+          # How far is the smallest mean distance to a different cluster?
+          # Inter-cluster distance from this point: 
+          
+          
+          
+
